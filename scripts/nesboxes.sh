65946bf64e5e788b4dc6c38395c564ffab887a68
Matthew Deutsch <flibidydibidy@gmail.com>
2025-01-19 13:56:38 -0700

    Committing the minor changes at gdq to the nesboxes script
    Eventually should do this properly..

--- scripts/nesboxes.sh
+++ scripts/nesboxes.sh
@@ -1,8 +1,13 @@
 tmux new-session -d 'ssh nesbox01' \; \
-    split-window -h -p 60 'ssh nesbox02' \; \
-    split-window -h -p 50 'ssh nesbox03' \; \
+    split-window -h -p 50 'ssh nesbox02' \; \
     bind-key X set-window-option synchronize-panes\; display-message "synchronize-panes is now #{?pane_synchronized,on,off}" \; \
     attach-session -d
+
+#tmux new-session -d 'ssh nesbox01' \; \
+#    split-window -h -p 60 'ssh nesbox02' \; \
+#    split-window -h -p 50 'ssh nesbox03' \; \
+#    bind-key X set-window-option synchronize-panes\; display-message "synchronize-panes is now #{?pane_synchronized,on,off}" \; \
+#    attach-session -d
 
 #tmux new-session -d 'ssh nesbox01' \; \
 #    split-window -h -p 75 'ssh nesbox02' \; \
